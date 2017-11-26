6a704ba84d4b1410c11b0acefd5b58613c3289e5
tschuchortdev <t.schuchort@googlemail.com>
2017-11-26 14:03:07 +0100

    sign in

--- app/src/main/java/com/tschuchort/soundshare/soundshare/MainActivity.kt
+++ app/src/main/java/com/tschuchort/soundshare/soundshare/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.tschuchort.soundshare.soundshare
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.ResultCodes
+import com.firebase.ui.auth.IdpResponse
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val signInRequestCode = 123
+    private var user: FirebaseUser? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -30,5 +41,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        signIn()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == signInRequestCode) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == ResultCodes.OK) {
+                // Successfully signed in
+                user = FirebaseAuth.getInstance().currentUser
+            }
+            else {
+                Toast.makeText(this, "failed to sign in", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signIn() {
+        val providers = listOf(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                signInRequestCode)
     }
 }
